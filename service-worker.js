d13044d90507bc2056b5deb151d63e6fefd59c6b
beaziz1 <kanekigo91@gmail.com>
2026-09-16 09:40:20 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+const CACHE_NAME="symmetry-drawing-v1";
+const FILES=["./","./index.html","./manifest.json","./service-worker.js","./icon-192.png"];
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES))));
+self.addEventListener("fetch",event=>event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request))));
